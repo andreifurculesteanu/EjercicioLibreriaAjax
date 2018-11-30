80cdd1422485c43646274406c06cb1db5cab5b7e
andreifurculesteanu <andrei.furcu@gmail.com>
2018-11-30 11:44:39 +0100

    First commit - Class libro added

---
+++ EjercicioLibreriaAjax/src/Objetos/Libro.java
@@ -0,0 +1,51 @@
+package Objetos;
+
+public class Libro {
+	
+	//Propiedades
+	String ISBN, titulo, autor;
+	int ano;
+	
+	
+	//Constructor
+	public Libro(String ISBN, String titulo, String autor, int ano) {
+		this.ISBN = ISBN;
+		this.titulo = titulo;
+		this.autor = autor;
+		this.ano = ano;
+	}
+
+
+	
+	public String getISBN() {
+		return ISBN;
+	}
+
+	public void setISBN(String iSBN) {
+		ISBN = iSBN;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+
+	public int getAno() {
+		return ano;
+	}
+
+	public void setAno(int ano) {
+		this.ano = ano;
+	}
+}
